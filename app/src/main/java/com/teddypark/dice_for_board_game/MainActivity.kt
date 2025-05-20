c153b13366e33079533047c324d0612d1dfc9c38
박태규 <teddy@bitbyte.kr>
2025-05-21 00:18:07 +0900

    banner_ads_init

--- app/src/main/java/com/teddypark/dice_for_board_game/MainActivity.kt
+++ app/src/main/java/com/teddypark/dice_for_board_game/MainActivity.kt
@@ -1,10 +1,21 @@
 package com.teddypark.dice_for_board_game
 
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.view.WindowMetrics
 import android.view.animation.AlphaAnimation
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 import com.teddypark.dice_for_board_game.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,11 +25,61 @@ class MainActivity : AppCompatActivity() {
     private val diceButtonList: MutableList<View> = mutableListOf()
     private val diceSumList: MutableList<Int> = MutableList(9) { 0 }
 
+    private var bannerAdView: AdView? = null
+
+    private val adSize: AdSize
+        get() {
+            val displayMetrics = resources.displayMetrics
+            val adWidthPixels =
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                    val windowMetrics: WindowMetrics = this.windowManager.currentWindowMetrics
+                    windowMetrics.bounds.width()
+                } else {
+                    displayMetrics.widthPixels
+                }
+            val density = displayMetrics.density
+            val adWidth = (adWidthPixels / density).toInt()
+            return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, adWidth)
+        }
+
+    override fun onPause() {
+        super.onPause()
+        bannerAdView?.pause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        bannerAdView?.resume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        bannerAdView?.destroy()
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        CoroutineScope(Dispatchers.IO).launch {
+            // Initialize the Google Mobile Ads SDK on a background thread.
+            MobileAds.initialize(this@MainActivity) {}
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content)) { view, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                systemBars.bottom
+            )
+            insets
+        }
+
+        initBannerAdView()
         getDiceButtonList()
         initDiceButton()
         initButton()
@@ -95,6 +156,22 @@ class MainActivity : AppCompatActivity() {
         binding.diceText4.text = diceSumList.count { it == 4 }.toString()
         binding.diceText5.text = diceSumList.count { it == 5 }.toString()
         binding.diceText6.text = diceSumList.count { it == 6 }.toString()
+    }
+
+    private fun initBannerAdView() {
+
+
+        // Create a new ad view.
+        bannerAdView = AdView(this)
+        bannerAdView?.adUnitId = "ca-app-pub-4608445788299748/6671701586"//"ca-app-pub-3940256099942544/9214589741"
+        bannerAdView?.setAdSize(adSize)
+
+        // Replace ad container with new ad view.
+        binding.adViewContainer.removeAllViews()
+        binding.adViewContainer.addView(bannerAdView)
+
+        val adRequest = AdRequest.Builder().build()
+        bannerAdView?.loadAd(adRequest)
     }
 
 }
